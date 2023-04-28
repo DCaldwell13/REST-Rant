50ba7b359a42568baae4bb9c24e69f2986eb3573
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-04-27 20:46:47 -0700

    REST-Rant

--- controllers/places.js
+++ controllers/places.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
+const places = require("../models/places.js")
 
-/*router.get('/', (req, res) => {
-    res.send('GET /places');
-});*/
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
-})
+  res.send('POST /places');
+});
 
 router.get('/new', (req, res) => {
-  res.render('places/new')
+  res.redirect('places/new')
 })
 
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.send('POST /places')
+});
 
 router.get('/', (req, res) => {
     let places = [{
